df74ea6b8e84771ee8cfeea9a07b995bfdc19c50
izzahsandusi11 <izzahsandusi11@gmail.com>
2024-06-23 21:47:49 +0800

    acap suruh

--- database/migrations/2024_06_23_130915_create_posts_table.php
+++ database/migrations/2024_06_23_130915_create_posts_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->text('content');
+            $table->foreignId('user_Id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
+
         });
     }
 
